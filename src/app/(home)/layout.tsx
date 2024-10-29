5014f2d9af71b0108fd815633e583ad1350295a4
kashif-lepton <kashif.khan@leptonmaps.com>
2024-10-29 07:16:27 +0530

    jg

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/ui/hero";
 import Sidebar from "@/components/ui/sidebar-main";
 import React from "react";
 
-export default function layout({ children }) {
+export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Sidebar />
