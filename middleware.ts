ab72f97ca1cde3ad8c56546dde8b1fb541f09866
kashif-lepton <kashif.khan@leptonmaps.com>
2024-10-29 07:11:51 +0530

    kjb

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { updateSession } from "./utils/supabase/middleware";
-
-export async function middleware(req) {
-  return await updateSession(req);
-}
-
-export const config = {
-  matcher: ["/((?!_next/static|favicon.ico).*)"],
-};
