83354eae56b6f8333c805e8cb3c3228c037038a2
kashif-lepton <kashif.khan@leptonmaps.com>
2024-11-06 19:11:31 +0530

    ccc

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 import React from "react";
-import AdminPanel from "./admin-panel";
+// import AdminPanel from "./admin-panel";
+import dynamic from "next/dynamic";
+const AdminPanel = dynamic(() => import("./admin-panel"), {
+  ssr: false,
+});
 
 export default function Admin() {
   return <AdminPanel />;
